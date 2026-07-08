a8ecfcc1eb337e77013baef4f56f8ffd90d1d700
infob2pinternational-ai <info.b2pinternational@gmail.com>
2026-07-08 18:12:23 +0530

    Update Vite config to include Tailwind CSS
    Removed proxy configuration and added Tailwind CSS plugin.

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/doc': {
-        target: 'https://document-gen-ten.vercel.app',
-        changeOrigin: true,
-      },
-      '/invoice': {
-        target: 'https://document-gen-ten.vercel.app',
-        changeOrigin: true,
-      },
-      '/receipt': {
-        target: 'https://document-gen-ten.vercel.app',
-        changeOrigin: true,
-      },
-      '/quotation': {
-        target: 'https://document-gen-ten.vercel.app',
-        changeOrigin: true,
-      }
-    }
-  }
-});
+  plugins: [react(), tailwindcss()],
+  build: {
+    sourcemap: true,
+  },
+})
